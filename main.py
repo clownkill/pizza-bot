577cb361f547a40b77a83720a53fd7a534c12a1d
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-07-14 23:45:06 +0300

    add created flows

--- main.py
+++ main.py
@@ -5,7 +5,30 @@ from pprint import pprint
 from dotenv import load_dotenv
 import requests
 
-from shop import get_client_token_info, add_product_image, create_product
+from shop import (get_client_token_info,
+                  add_product_image,
+                  create_product,
+                  add_flows_and_get_id
+                  )
+
+
+def get_addresses():
+    with open('pizzas_json/addresses.json', 'r') as fin:
+        addresses = json.load(fin)
+
+    return addresses
+
+
+def get_menu():
+    with open('pizzas_json/menu.json', 'r') as fin:
+        menu = json.load(fin)
+
+    return menu
+
+
+def add_products(token, menu):
+    for product in menu:
+        create_product(token, product)
 
 
 if __name__ == '__main__':
@@ -15,12 +38,12 @@ if __name__ == '__main__':
     grant_type = os.getenv('GRANT_TYPE')
     tg_token = os.getenv("TELEGRAM_TOKEN")
 
-    with open('pizzas_json/addresses.json', 'r') as fin:
-        addresses = json.load(fin)
-
-    with open('pizzas_json/menu.json', 'r') as fin:
-        menu = json.load(fin)
+    address = get_addresses()
+    menu = get_menu()
 
     token = get_client_token_info(client_id, client_secret, grant_type)['access_token']
-    for product in menu:
-        create_product(token, product)
+
+    name = 'Pizzeria'
+    description = 'Pizzeria with address and coordinates'
+    flow_id = add_flows_and_get_id(token, name, description)
+
--- shop.py
+++ shop.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pprint import pprint
+from uuid import uuid4
 
 from dotenv import load_dotenv
 import requests
@@ -60,7 +61,7 @@ def create_product(token, product):
         'data': {
             'type': 'product',
             'name': product['name'],
-            'slug': 'sl' + str(product['id']),
+            'slug': str(uuid4())[-12:],
             'sku': 'sk' + str(product['id']),
             'description': product['description'],
             'manage_stock': True,
@@ -85,3 +86,69 @@ def create_product(token, product):
     product_image_id = upload_product_image(token, product_image_url)
 
     add_product_image(token, stored_product_id, product_image_id)
+
+
+def add_flows_and_get_id(token, name, description):
+    url = 'https://api.moltin.com/v2/flows'
+    headers = {
+        'Authorization': f'{token}',
+    }
+
+    json_data = {
+        'data': {
+            'type': 'flow',
+            'name': name,
+            'slug': str(uuid4())[-12:],
+            'description': description,
+            'enabled': True,
+        },
+    }
+
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
+
+    flows = response.json()
+
+    return flows['data']['id']
+
+
+def create_flows_field(token, flow_id, field_name, field_type):
+    url = 'https://api.moltin.com/v2/fields'
+    headers = {
+        'Authorization': f'{token}',
+    }
+
+    json_data = {
+        'data': {
+            'type': 'field',
+            'name': 'Product Rating',
+            'slug': 'product-rating',
+            'field_type': 'integer',
+            'validation_rules': [
+                {
+                    'type': 'between',
+                    'options': {
+                        'from': 1,
+                        'to': 5,
+                    },
+                },
+            ],
+            'description': 'Average rating as given by our users',
+            'required': False,
+            'default': 0,
+            'enabled': True,
+            'order': 1,
+            'omit_null': False,
+            'relationships': {
+                'flow': {
+                    'data': {
+                        'type': 'flow',
+                        'id': flow_id,
+                    },
+                },
+            },
+        },
+    }
+
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
