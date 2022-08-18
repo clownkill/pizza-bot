820fb7d0b21b922d5216bea090747546db5e7cf2
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-08-18 15:45:56 +0300

    add state machine

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
-from pprint import pprint
-import sys
-import json
-from datetime import datetime
+import logging
+from functools import partial
 
 import requests
+import redis
 from flask import Flask, request
 from dotenv import load_dotenv
 
@@ -18,11 +17,21 @@ from shop import (
 )
 
 load_dotenv()
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-grant_type = os.getenv("GRANT_TYPE")
 
 app = Flask(__name__)
+
+DATABASE = None
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def error(state, error):
+    logger.warning(f"State {state} caused error {error}")
 
 
 @app.route("/", methods=["GET"])
@@ -30,7 +39,9 @@ def verify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get(
         "hub.challenge"
     ):
-        if not (request.args.get("hub.verify_token") == os.getenv("VERIFY_TOKEN")):
+        if not (
+            request.args.get("hub.verify_token") == os.getenv("VERIFY_TOKEN")
+        ):
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -39,59 +50,24 @@ def verify():
 
 @app.route("/", methods=["POST"])
 def webhook():
-    data = request.get_json()
-    log(data)
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    grant_type = os.getenv("GRANT_TYPE")
     access_token = get_client_token_info(client_id, client_secret, grant_type)[
         "access_token"
     ]
 
+    data = request.get_json()
     if data["object"] == "page":
-
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-
                 if messaging_event.get("message"):
-
                     sender_id = messaging_event["sender"]["id"]
-                    recipient_id = messaging_event["recipient"]["id"]
                     message_text = messaging_event["message"]["text"]
-                    
-                    send_menu(sender_id, access_token)
 
-                if messaging_event.get("delivery"):
-                    pass
-
-                if messaging_event.get("optin"):
-                    pass
-
-                if messaging_event.get("postback"):
-                    pass
+                    handle_users_reply(sender_id, message_text, access_token)
 
     return "ok", 200
-
-
-def send_message(recipient_id, message_text):
-
-    log(
-        "sending message to {recipient}: {text}".format(
-            recipient=recipient_id, text=message_text
-        )
-    )
-
-    params = {"access_token": os.getenv("PAGE_ACCESS_TOKEN")}
-    headers = {"Content-Type": "application/json"}
-    data = json.dumps(
-        {"recipient": {"id": recipient_id}, "message": {"text": message_text}}
-    )
-    response = requests.post(
-        "https://graph.facebook.com/v2.6/me/messages",
-        params=params,
-        headers=headers,
-        data=data,
-    )
-    if response.status_code != 200:
-        log(response.status_code)
-        log(response.text)
 
 
 def get_menu_elemets(access_token):
@@ -146,6 +122,8 @@ def get_menu_elemets(access_token):
             },
         )
 
+    subtitle_text = "Остальные пиццы можно посмотреть в одной из категорий"
+    iamge_url = "https://primepizza.ru/uploads/position/large_0c07c6fd5c4dcadddaf4a2f1a2c218760b20c396.jpg"
     category_buttons = []
     for category in categories:
         if category != "basic":
@@ -162,8 +140,8 @@ def get_menu_elemets(access_token):
     elements.append(
         {
             "title": "Не нашли нужную пиццу?",
-            "image_url": "https://primepizza.ru/uploads/position/large_0c07c6fd5c4dcadddaf4a2f1a2c218760b20c396.jpg",
-            "subtitle": "Остальные пиццы можно посмотреть в одной из категорий",
+            "image_url": iamge_url,
+            "subtitle": subtitle_text,
             "buttons": category_buttons,
         },
     )
@@ -171,7 +149,7 @@ def get_menu_elemets(access_token):
     return elements
 
 
-def send_menu(recipient_id, access_token):
+def send_menu(sender_id, access_token):
     headers = {
         "Content-Type": "application/json",
     }
@@ -182,7 +160,7 @@ def send_menu(recipient_id, access_token):
 
     json_data = {
         "recipient": {
-            "id": recipient_id,
+            "id": sender_id,
         },
         "message": {
             "attachment": {
@@ -201,22 +179,50 @@ def send_menu(recipient_id, access_token):
         headers=headers,
         json=json_data,
     )
-
-    if response.status_code != 200:
-        log(response.status_code)
-        log(response.text)
+    response.raise_for_status()
 
 
-def log(msg, *args, **kwargs):
+def handle_start(sender_id, message_text, access_token):
+    send_menu(sender_id, access_token)
+
+    return "MENU"
+
+
+def handle_users_reply(sender_id, message_text, access_token):
+    db = get_database_connection()
+
+    states_functions = {
+        "START": partial(handle_start, access_token=access_token),
+    }
+
+    recorded_state = db.get(f"facebookid_{sender_id}")
+    if not recorded_state or recorded_state not in states_functions.keys():
+        user_state = "START"
+    else:
+        user_state = recorded_state
+    if message_text == "/start":
+        user_state = "START"
+    state_handler = states_functions[user_state]
     try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
-        else:
-            msg = msg
-        print("{}: {}".format(datetime.now(), msg))
-    except UnicodeEncodeError:
-        pass
-    sys.stdout.flush()
+        next_state = state_handler(sender_id, message_text)
+        db.set(f"facebookid_{sender_id}", next_state)
+    except Exception as err:
+        error(user_state, err)
+
+
+def get_database_connection():
+    global DATABASE
+    if DATABASE is None:
+        database_password = os.getenv("DATABASE_PASSWORD")
+        database_host = os.getenv("DATABASE_HOST")
+        database_port = os.getenv("DATABASE_PORT")
+        DATABASE = redis.Redis(
+            host=database_host,
+            port=database_port,
+            password=database_password,
+            decode_responses=True,
+        )
+    return DATABASE
 
 
 if __name__ == "__main__":
